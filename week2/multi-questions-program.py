7b194e18fc6f19ead9dc510830124922a5401bc8
Kakashi-93 <37810637+Kakashi-93@users.noreply.github.com>
2020-01-18 20:36:01 +0300

    Learn Turkish in 3 days

--- week2/multi-questions-program.py
+++ week2/multi-questions-program.py
@@ -59,18 +59,45 @@
 
 
 #------------------ @start:khalid------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
+""" learn Turkish in 3 days!!
+It's a lie, you can't"""
+enter = input('click Enter to continue \n')
+question = 0
+while True:
+    if question == 0:
+        print("""What does 'Araba' mean in Turkish? 
+        1) Book
+        2) Car
+        3) Orange
+        """)
+        choice = input("What is your choice? ").lower()
+        question = 1
+    elif choice == 'car':
+        print("Your Turkish language is good, keep learning.")
+        print('*' * 30)
+        choice = ''
+        question = 2
+    elif question == 2:
+        print("""What does 'Kitab' mean in Turkish? 
+                1) Book
+                2) Car
+                3) Orange
+                """)
+        choice = input("What is your choice? ").lower()
+        question = 3
+    elif choice == 'book':
+        print("Your Turkish language is good, keep learning.")
+        print('*' * 30)
+        choice =''
+        question = 3
+    elif question == 3:
+        print("Your Turkish is amazing!")
+        print("Now you just learned 2 words")
+        break
+    else:
+        print('Sorry, try again')
+        print('*' * 30)
+        question = 0
 #-------------------- @end:khalid------------------------------
 
 
